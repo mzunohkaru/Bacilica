117305357ae6da082599c7d4f09828b289418f28
mzunohkaru <winkaisyu@gmail.com>
2024-09-07 22:10:09 +0900

    setup:server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,31 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { serve } from "@hono/node-server";
+
+const app = new Hono().basePath("/api");
+
+app.use(cors());
+app.use(logger());
+app.use(prettyJSON());
+app.notFound((c) => c.text("404 Not found"));
+
+app.get("/", (c) => c.text("Hello, Hono!"));
+
+app.onError((err, c) => {
+  console.error(err);
+  return c.json({ message: err.message }, 500);
+});
+
+const PORT = process.env.PORT || 8080;
+
+serve({
+  fetch: app.fetch,
+  port: Number(PORT),
+});
+
+console.log(`Server is running at http://localhost:${PORT}/api`);
+
+export default app;
+export type AppType = typeof app;
