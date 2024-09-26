2f84365fe1957e52da477aba9fa1d73cca46ca9f
mzunohkaru <mizuno.hikaru.01@gmail.com>
2024-09-27 00:40:09 +0900

    style:format

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,7 +2,7 @@
 
 import { useUser } from '@/hook/useUser'
 import { usePost } from '@/hook/usePost'
-import { ReloadIcon } from "@radix-ui/react-icons"
+import { ReloadIcon } from '@radix-ui/react-icons'
 
 import { Button } from '@/components/ui/button'
 
@@ -25,13 +25,17 @@ export function App() {
   return (
     <div>
       <main>
-        <h1 className="text-2xl font-bold text-center">Client</h1>
-        <Button variant="secondary" onClick={() => handleFetchUser()}>Fetch User</Button>
-        <Button variant="default" onClick={() => handleCreateUser()}>Create User</Button>
+        <h1 className='text-2xl font-bold text-center'>Client</h1>
+        <Button variant='secondary' onClick={() => handleFetchUser()}>
+          Fetch User
+        </Button>
+        <Button variant='default' onClick={() => handleCreateUser()}>
+          Create User
+        </Button>
         <Button disabled>
-      <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-      Please wait
-    </Button>
+          <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
+          Please wait
+        </Button>
         <div>
           <p>userData: {JSON.stringify(userData)}</p>
           <p>isMutating: {JSON.stringify(isMutating)}</p>
@@ -39,7 +43,7 @@ export function App() {
         </div>
       </main>
       <footer>
-        <div className="bg-red-500 text-white p-2 rounded-md">
+        <div className='bg-red-500 text-white p-2 rounded-md'>
           <p>postData: {JSON.stringify(postData)}</p>
         </div>
       </footer>
