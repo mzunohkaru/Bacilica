59f17414da17fdc6d0c27131de4445fed746d4ef
mzunohkaru <mizuno.hikaru.01@gmail.com>
2024-09-27 00:38:08 +0900

    test:shadcn button component

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,6 +2,9 @@
 
 import { useUser } from '@/hook/useUser'
 import { usePost } from '@/hook/usePost'
+import { ReloadIcon } from "@radix-ui/react-icons"
+
+import { Button } from '@/components/ui/button'
 
 export function App() {
   const { userData, fetchUser, isMutating, error, createUserData, createUser } =
@@ -23,8 +26,12 @@ export function App() {
     <div>
       <main>
         <h1 className="text-2xl font-bold text-center">Client</h1>
-        <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => handleFetchUser()}>Fetch User</button>
-        <button className="bg-green-500 text-white p-2 rounded-md" onClick={() => handleCreateUser()}>Create User</button>
+        <Button variant="secondary" onClick={() => handleFetchUser()}>Fetch User</Button>
+        <Button variant="default" onClick={() => handleCreateUser()}>Create User</Button>
+        <Button disabled>
+      <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+      Please wait
+    </Button>
         <div>
           <p>userData: {JSON.stringify(userData)}</p>
           <p>isMutating: {JSON.stringify(isMutating)}</p>
