93b9e81efad71678f212fb4827af9da4aa51a9b1
mzunohkaru <mizuno.hikaru.01@gmail.com>
2024-10-02 00:54:18 +0900

    fix:hono api chain

--- apps/server/src/router/user.ts
+++ apps/server/src/router/user.ts
@@ -6,9 +6,7 @@ import { UserResponse } from '@repo/schema'
 const app = new Hono().get(async c => {
   const users: UserResponse[] = await prisma.user.findMany()
   return c.json(users)
-})
-
-app.post(async c => {
+}).post(async c => {
   const body = await c.req.json()
   const user: UserResponse = await prisma.user.create({
     data: body,
