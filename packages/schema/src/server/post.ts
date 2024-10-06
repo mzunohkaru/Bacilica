a279beec0f8318b265512ee0a6d0a670bbe9da37
mizuno-hikaru <mizuno.hikaru.01@gmail.com>
2024-10-06 13:24:06 +0900

    delete:post sample

--- packages/schema/src/server/post.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod'
-
-export const PostResponse = z.object({
-  id: z.string(),
-  title: z.string(),
-  content: z.string(),
-})
-
-export type PostResponse = z.infer<typeof PostResponse>
