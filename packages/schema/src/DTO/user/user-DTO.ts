4381feb9cd5f52ba0665a09dc15351d3bc19adff
mizuno-hikaru <mizuno.hikaru.01@gmail.com>
2024-11-02 13:12:59 +0900

    update:utils schema

--- packages/schema/src/DTO/user/user-DTO.ts
+++ packages/schema/src/DTO/user/user-DTO.ts
@@ -1,10 +1,17 @@
 import z from 'zod'
 
+import { USER_TYPE } from '@repo/schema/src/utils'
+
 export const UserDTO = z.object({
   session: z.string().uuid(),
   userName: z.string(),
   avatarUrl: z.string().nullish(),
-  userTypeId: z.number(),
+  userType: z.enum([
+    USER_TYPE.JUNIOR,
+    USER_TYPE.SENIOR,
+    USER_TYPE.PROFESSIONAL,
+    USER_TYPE.ADMIN,
+  ]),
   invitedTicket: z.number().default(0),
   invitedToken: z.string().nullish(),
   githubUrl: z.string().nullish(),
--- packages/schema/src/server/user/user.ts
+++ packages/schema/src/server/user/user.ts
@@ -1,25 +1,19 @@
 import z from 'zod'
 
+import { USER_TYPE } from '@repo/schema/src/utils'
 export const UserRequest = z.object({
   uid: z.string().uuid(),
   userName: z.string(),
   avatarUrl: z.string().nullish(),
-  userType: z.string(),
+  userType: z.enum([
+    USER_TYPE.JUNIOR,
+    USER_TYPE.SENIOR,
+    USER_TYPE.PROFESSIONAL,
+    USER_TYPE.ADMIN,
+  ]),
   invitedTicket: z.number().positive().default(0),
   invitedToken: z.string().nullish(),
   githubUrl: z.string().nullish(),
 })
 
 export type UserRequest = z.infer<typeof UserRequest>
-
-export const UserResponse = z.object({
-  uid: z.string().uuid(),
-  userName: z.string(),
-  avatarUrl: z.string().nullish(),
-  userType: z.string(),
-  invitedTicket: z.number().default(0),
-  invitedToken: z.string().nullish(),
-  githubUrl: z.string().nullish(),
-})
-
-export type UserResponse = z.infer<typeof UserResponse>
